638a77e002d383abcbe9354bb8436c653afa9466
Mario Mitic <mariomitic@yahoo.com>
2023-12-21 16:37:56 -0800

    split third try

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; 
-// const router = jsonServer.router("db.json");
 
-const router = jsonServer.router();
+
+const router1 = jsonServer.router();
 const db_accountlist = require("./db_accountlist.json");
-router.db._.assign(db_accountlist);
+router1.db._.assign({ accountlist: db_accountlist });
 
 const router2 = jsonServer.router();
 const db_recipes = require("./db_recipes.json");
-router2.db._.assign(db_recipes);
+router2.db._.assign({ recipes: db_recipes });
 
 const router3 = jsonServer.router();
 const db_daily = require("./db_daily.json");
-router3.db._.assign(db_daily);
+router3.db._.assign({ daily: db_daily });
 
-server.use("/data1", router);
+server.use("/data1", router1);
 server.use("/data2", router2);
 server.use("/data3", router3);
 
 server.use(middlewares);
-//server.use(router);
+
 
 server.listen(port);
