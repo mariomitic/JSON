2d1bf17a5956419ecf0d5d6aebc23dfa36f44c50
Mario Mitic <mariomitic@yahoo.com>
2023-12-21 16:46:29 -0800

    split four

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; 
 
 
-const router1 = jsonServer.router();
+
 const db_accountlist = require("./db_accountlist.json");
-router1.db._.assign({ accountlist: db_accountlist });
+const router1 = jsonServer.router({ accountlist: db_accountlist });
 
-const router2 = jsonServer.router();
+
 const db_recipes = require("./db_recipes.json");
-router2.db._.assign({ recipes: db_recipes });
+const router2 = jsonServer.router({ recipes: db_recipes });
 
-const router3 = jsonServer.router();
+
 const db_daily = require("./db_daily.json");
-router3.db._.assign({ daily: db_daily });
+const router3 = jsonServer.router({ daily: db_daily });
 
 server.use("/data1", router1);
 server.use("/data2", router2);
